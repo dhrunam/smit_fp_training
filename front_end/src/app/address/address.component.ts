5619be877644b772ad8b1709f5c290c547870bcd
zetsu des <ankitchettri098@gmail.com>
2024-02-07 12:59:42 +0530

    first time add backed

---
+++ front_end/src/app/address/address.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-address',
+  templateUrl: './address.component.html',
+  styleUrl: './address.component.css'
+})
+export class AddressComponent {
+
+  address={
+    name: "Ankit",
+    phone: "9878767589",
+  }
+
+}
--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddressComponent } from './address/address.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
